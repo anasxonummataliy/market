41d37dd7cd9e1d8b1f0bbc8e5985b4e8f9ee3e5f
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-08-28 17:16:03 +0500

    added models and fix problems

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+from app.core.config import conf
+from app.models import db, migrate, bcrypt, login_manager
+from app.routes import register_all_blueprints
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY=conf.secret_key,
+        WTF_CSRF_SECRET_KEY=conf.wtf_csrf_secret_key,
+        SQLALCHEMY_DATABASE_URI=conf.db_uri,
+    )
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager(app)
+
+    register_all_blueprints(app)
+
+    return app
